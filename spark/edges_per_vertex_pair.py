fbccc79008e812c47af67a48fcde256bc540bc37
Martin <venturamartin90@gmail.com>
2017-10-10 01:45:07 -0300

    finished spark edge count pairs exercise

--- spark/edges_per_vertex_pair.py
+++ spark/edges_per_vertex_pair.py
@@ -12,14 +12,51 @@ sc = SparkContext()
 sc.setLogLevel("ERROR")
 
 edges = sc.textFile(sys.argv[1])
-print(edges.collect())
 edges = edges.map(lambda x: splitToOrderedTuple(x, ','))
-print(edges.collect())
 edges = edges.distinct() 
-print(edges.collect())
 if not self_loops:
 	edges = edges.filter(lambda x: x[0] != x[1])
-edges = edges.map(lambda x: )
+
+cant1 = edges.map(lambda x: (x[0],x))
+cant2 = edges.map(lambda x: (x[1],x))
+
+res = sc.union([cant1,cant2])
+
+res = res.filter(lambda x: type(x[1]) == tuple)
+
+res = res.map(lambda x: (x[0],[x[1]]))
+res = res.reduceByKey(lambda x, y: x+y)
+
+def defCant(x):
+	var = x[0]
+	list = x[1]
+	cants = []
+	for e in list:
+		if e[0] == var:
+			cants = cants + [(len(list), 0)]
+		else:
+			cants = cants + [(0, len(list))]
+	return (var,(list,cants))
+
+res = res.map(lambda x: defCant(x))
+
+def toTuples(x):
+	lists = x[1]
+	tmp = zip(lists[0], lists[1])
+	res = []
+	for a, b in tmp:
+		res = res + [(a,b)]
+	return tuple(res)
+
+res = res.flatMap(lambda x: toTuples(x))
+
+
+def reducePairs(x,y):
+	return (x[0] + y[0], x[1] + y[1]) 
+
+
+res = res.reduceByKey(lambda x, y: reducePairs(x, y))
+
 
 print(' _____________________________________________________________________________________  ')
 print('|                                                                                     | ')
@@ -27,4 +64,4 @@ print('|                                   R E S U L T S                        
 print('|_____________________________________________________________________________________||')
 print(' \_____________________________________________________________________________________|')
 
-print(edges.collect())
+print(res.collect())
