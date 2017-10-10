a386028f9d4fef2cec4a559dfa01ad7c032a92d0
Martin <venturamartin90@gmail.com>
2017-10-10 01:45:37 -0300

    small fix in freq_words

--- spark/freq_words.py
+++ spark/freq_words.py
@@ -1,4 +1,6 @@
+from __future__ import division
 from pyspark import SparkContext
+
 import sys
 
 sc = SparkContext()
@@ -13,16 +15,14 @@ word_cant = words.map(lambda x: (x,1))
 total = words.count()
 
 cant_by_word = word_cant.reduceByKey(lambda x, y: x + y)
-#Si uysara reduce normal se puede hacer lambda (None, x[1] + y[1])
-
+#Si usara reduce normal se puede hacer lambda (None, x[1] + y[1])
 
 freq = cant_by_word.map(lambda x: (x[0],x[1]/total))
 #map usa una sola entrada. Pero puede tener varias de salida.
 
-sortedFreq = freq.sortBy(lambda x: x[1], false)
+sortedFreq = freq.sortBy(lambda x: x[1], False)
 
-#out = freq.takeOrdered(5, lambda s: -1*s)
-out.take(10)
+out = freq.takeOrdered(5, lambda s: -1*s)
 
 print(' ___________________________________')
 print('|              RESULTS              |')
